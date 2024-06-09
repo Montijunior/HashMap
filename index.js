2cb39525d4922da5d2a14854c93af607e307a2c1
Monti junior <montiabouou@gmail.com>
2024-06-08 20:58:28 -0800

    hash code function created

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // run script with Node <filename>.js
+class HashMap {
+  constructor(size) {
+    this.buckets = [];
+    this.size = size;
+  }
+
+  // hash code method
+  hash(key) {
+    let hashCode = 0;
+    let primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.size;
+    }
+    return hashCode;
+  }
+}
+
+const map = new HashMap(16);
+console.log(map.hash("Junior"));
